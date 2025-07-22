27aeb15ad42e99b88ce6d724a0302c1e29178110
Martijn van der Velde <109382272+desk-dev@users.noreply.github.com>
2025-07-22 15:19:43 +0200

    Add files via upload
    Kilometervergoeding bereken project

---
+++ kilometervergoeding-berekenen.py
@@ -0,0 +1,18 @@
+while True:
+    try:
+        zakelijke_kilometers = float(input("Aantal zakelijke kilometers in het afgelopen jaar: "))
+        if zakelijke_kilometers < 0:
+            raise ValueError("Aantal kilometers kan niet negatief zijn.")
+        break
+    except ValueError as e:
+        print(f"Ongeldige invoer: {e}. Probeer opnieuw.")
+kilometervergoeding = 0.23 # vergoeding in 2024
+
+def kilometervergoeding_berekenen(zakelijke_kilometers,kilometervergoeding):
+    """ Het berekenen van je gereden zakelijke kilometers met je prive auto van het afgelopen boekjaar"""
+    vergoeding = zakelijke_kilometers * kilometervergoeding
+    print(f"Je totale zakelijke kilometers: €{zakelijke_kilometers:.2f} ")
+    print(f"Je vergoeding die mag meenemen in de kosten van je boekhouding: €{vergoeding:.2f}")
+
+kilometervergoeding_berekenen(zakelijke_kilometers,kilometervergoeding)
+    
